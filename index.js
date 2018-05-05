88c69e9854fcf5ee54b30caf9f50f72e0b90a663
Patryk Kurzeja <patrik.kurzeja@gmail.com>
2018-05-05 19:01:36 +0200

    backend done

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express'),
+
+    http = require('http'),
+    socketIo = require('socket.io'),
+    app = express();
+    server = http.createServer(app),
+    io = socketIo(server),
+    UserService = require('./UsersService'),
+    userService = new UserService();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+io.on('connection', (socket)=> {
+    socket.on('message', (message)=> {
+        const {name} = userService.getUserById(socket.id);
+        socket.broadcast.emit('message', {
+            text: message.text,
+            from: name
+        });
+    });
+socket.on('disconnect', ()=> {
+    userService.removeUser(socket.id);
+    socket.broadcast.emit('update', {
+        users: userService.getAllusers()
+    });
+});
+    //client is listening for entry info
+socket.on('join', (name)=>{
+    //new user is saved with list of contacts
+    userService.addUser({
+        id: socket.id,
+        name
+    });
+    io.emit('update', {
+        users: userService.getAllusers()
+    });
+});
+});
+server.listen(3000,() => {
+    console.log('listening on *:3000');
+});
